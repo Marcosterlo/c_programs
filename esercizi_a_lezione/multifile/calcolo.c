d377f7042dac16feb66677105bd77f61e8c4f164
Marco Sterlini <marcosterlo@gmail.com>
2020-05-25 12:42:01 +0200

    Multifile

---
+++ esercizi_a_lezione/multifile/calcolo.c
@@ -0,0 +1,15 @@
+float somma(float a, float b) {
+    return a+b;
+};
+
+float differenza(float a, float b) {
+    return a-b;
+};
+
+float prodotto(float a, float b) {
+    return a*b;
+};
+
+float divisione(float a, float b) {
+    return a/b;
+};
---
+++ esercizi_a_lezione/multifile/calcolo.h
@@ -0,0 +1,4 @@
+extern float somma(float a, float b);
+extern float differenza(float a, float b);
+extern float prodotto(float a, float b);
+extern float divisione(float a, float b);
